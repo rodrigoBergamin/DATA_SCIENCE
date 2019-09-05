f06e960c701a24633a5fa55c2ff09045ff95083c
rodrigoBergamin <rb.bergamin@gmail.com>
2019-09-04 21:04:30 -0300

    Update listaDeAmigos.py

--- listaDeAmigos.py
+++ listaDeAmigos.py
@@ -1,4 +1,5 @@
-﻿users = [
+﻿from __future__ import division
+users = [
   {"id":566, "nome":"Rodrigo"},
   {"id":21, "nome":"Cristina"},
   {"id":3258, "nome":"Alessandra"},
@@ -18,7 +19,28 @@ for user in users:
   user["friends"] = []
 
 for u,f in friendship:
-  users[u]["friends"].append(users[f]["nome"])
-  users[f]["friends"].append(users[u]["nome"])
+  users[u]["friends"].append(users[f])
+  users[f]["friends"].append(users[u])
 
-print(users[0])
+
+def number_of_friends(user): #quantos amigos o usuário tem?
+  return len(user["friends"]) #retornando o tamanho da lista de amigos do usuário
+
+total_connections = sum(number_of_friends(user) for user in users) #retornará o total de conexões de cada usuário em users
+num_users = len(users) #tamanho da lista de usuários
+avg_connections = total_connections/num_users #total de conexões de todos os usuários divido pelo número de usuários
+
+
+num_friends_by_id = [ (user['id'],  number_of_friends(user))
+  for user in users]
+
+new_list = sorted(num_friends_by_id, #método sorted, que retornará um novo array ordenado pelo
+       key= lambda u: u[1], #número de amigos de cada usuário
+       reverse=True) #em ordem decrescente
+
+print(new_list)
+
+'''add = lambda x, y, z: x + y + z   // recebe argumentos e manipula esses argumentos
+
+print(add(3, 5, 8)) exemplo de lambda, função anonima
+'''
